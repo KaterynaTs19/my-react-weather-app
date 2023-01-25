f1c2cd4a0f86d60347fddafd7d1ef3fb2596aa93
KaterynaTs19 <tskatrinka@gmail.com>
2023-01-25 10:59:45 +0100

    added link to repository GitHub and personal page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <p>
+        This project was created by{" "}
+        <a href="" target="_blank">
+          Katia Tsykanova
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a href="" target="_blank">
+          GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
