fe3f5399b822e6110d5640ce1261518a65ead233
KaterynaTs19 <tskatrinka@gmail.com>
2023-01-29 21:37:38 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <p>
-        This project was created by{" "}
-        <a
-          href="https://jade-daifuku-97088b.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Katia Tsykanova
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/KaterynaTs19/my-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather app</h1>
+        <p>
+          This project was created by{" "}
+          <a
+            href="https://jade-daifuku-97088b.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Katia Tsykanova
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/KaterynaTs19/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
